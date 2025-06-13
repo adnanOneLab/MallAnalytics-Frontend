59a6d77948f6fb0fe7273f3f36c7c003031be93c
VenkateshSunke <venkatesh.sunke@onelabventures.com>
2025-06-13 11:57:35 +0530

    adding code for protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import VisitorsList from "./VisitorsList";
 import Dashboard from "./components/Dashboard";
 import RegistrationForm from "./RegistrationForm";
+
+// Sample user object for now
+const user = {
+  id: "u123",
+  name: "John Doe",
+  isSubscribed: true, // set true for subscribed users
+};
+
+const ProtectedRoute = ({ isAllowed, redirectTo, children }) => {
+  return isAllowed ? children : <Navigate to={redirectTo} replace />;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/visitors" element={<VisitorsList />} />
-        <Route path="/register-user" element={<RegistrationForm />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute isAllowed={user.isSubscribed} redirectTo="/register-user">
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/visitors"
+          element={
+            <ProtectedRoute isAllowed={user.isSubscribed} redirectTo="/register-user">
+              <VisitorsList />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/register-user"
+          element={
+            <ProtectedRoute isAllowed={!user.isSubscribed} redirectTo="/">
+              <RegistrationForm />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
