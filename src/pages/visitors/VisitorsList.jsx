60ccaea973ede3561d61622c0892be97c630baac
VenkateshSunke <venkatesh.sunke@onelabventures.com>
2025-06-23 16:11:46 +0530

    fixed ui bugs

--- src/pages/visitors/VisitorsList.jsx
+++ src/pages/visitors/VisitorsList.jsx
@@ -213,7 +213,9 @@ const VisitorsList = () => {
                     colSpan="8"
                     className="px-6 py-8 text-center text-gray-500"
                   >
-                    Loading visitors...
+                    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+                      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+                    </div>
                   </td>
                 </tr>
               ) : (
