56b1423ba28d97c855b60552da70c29374b72c39
venkatesh.sunke <venkatesh.sunke@onelabventures.com>
2025-06-27 18:54:57 +0530

    bug fix3

--- src/pages/visitors/AddContactModal.jsx
+++ src/pages/visitors/AddContactModal.jsx
@@ -33,7 +33,7 @@ export default function AddContactModal({ isOpen, onClose, selectedVisitors }) {
       api
         .get("api/campaigns/")
         .then((res) => {
-          setCampaigns(res.data);
+          setCampaigns(res.data.results);
         })
         .catch((err) => {
           console.error("Error fetching campaigns:", err);
