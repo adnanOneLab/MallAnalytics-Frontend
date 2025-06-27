1c4e20237ae243b702fc6d3345ae9407fdcfae3c
ayushnaik <ayush@onelabventures.com>
2025-06-27 19:42:17 +0530

    face detc

--- src/utils/faceDetection.js
+++ src/utils/faceDetection.js
@@ -3,10 +3,10 @@ import * as faceapi from 'face-api.js';
 export const loadFaceDetectionModels = async () => {
   try {
     await Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models')
+      faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('../models')
     ]);
     return true;
   } catch (error) {
